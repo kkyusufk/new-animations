5633808ea7d173fca1924400907d10fc11870109
Yusuf Khan <yusufk@Administrators-Mac-mini.local>
2021-03-19 23:11:44 +0530

    first draft of scoll animation

--- src/pages/scrollEffect.js
+++ src/pages/scrollEffect.js
@@ -1,41 +1,13 @@
-import * as React from 'react';
+import React from 'react';
+import { ScrollComponent } from '../components/scrollComponent';
 
-const paragraph1 = {
-    width: '100%',
-    fontSize: '40px'
-}
+const scrollEffect = () => {
 
-const scrollEffect = () => (
+  return (
     <main>
-        <div className="container">
-            <p style={paragraph1}>
-                lorAliqua dolor pariatur consequat ad irure reprehenderit aliqua consectetur. Ea nulla ullamco cillum sint adipisicing est reprehenderit sint incididunt eu mollit ullamco. Est duis minim qui laboris enim eiusmod. Sunt in qui cupidatat cupidatat ex labore reprehenderit incididunt tempor quis laboris. Aliquip qui irure enim et cillum.
-            </p>
-            <p style={paragraph1}>
-                Nulla dolor reprehenderit magna qui esse duis culpa consequat cillum do esse deserunt aute. Exercitation ex veniam cillum enim non aliquip cupidatat dolor consequat magna id. Adipisicing laborum laboris reprehenderit consequat duis dolore cupidatat eu. Laboris in minim amet ut dolore duis duis officia consectetur consectetur et cillum. Dolore est sit labore enim. Velit sint aliquip amet nisi non proident cillum incididunt laborum.
-            </p>
-            <p style={paragraph1}>
-                Nulla dolor reprehenderit magna qui esse duis culpa consequat cillum do esse deserunt aute. Exercitation ex veniam cillum enim non aliquip cupidatat dolor consequat magna id. Adipisicing laborum laboris reprehenderit consequat duis dolore cupidatat eu. Laboris in minim amet ut dolore duis duis officia consectetur consectetur et cillum. Dolore est sit labore enim. Velit sint aliquip amet nisi non proident cillum incididunt laborum.
-            </p>
-            <p style={paragraph1}>
-                Nulla dolor reprehenderit magna qui esse duis culpa consequat cillum do esse deserunt aute. Exercitation ex veniam cillum enim non aliquip cupidatat dolor consequat magna id. Adipisicing laborum laboris reprehenderit consequat duis dolore cupidatat eu. Laboris in minim amet ut dolore duis duis officia consectetur consectetur et cillum. Dolore est sit labore enim. Velit sint aliquip amet nisi non proident cillum incididunt laborum.
-            </p>
-            <p 
-                data-sal="flip-up"
-                data-sal-delay="300"
-                data-sal-easing="ease"
-                data-sal-repeat="true"
-            style={paragraph1}>
-                Nulla dolor reprehenderit magna qui esse duis culpa consequat cillum do esse deserunt aute. Exercitation ex veniam cillum enim non aliquip cupidatat dolor consequat magna id. Adipisicing laborum laboris reprehenderit consequat duis dolore cupidatat eu. Laboris in minim amet ut dolore duis duis officia consectetur consectetur et cillum. Dolore est sit labore enim. Velit sint aliquip amet nisi non proident cillum incididunt laborum.
-            </p>
-            <p style={paragraph1}>
-                Nulla dolor reprehenderit magna qui esse duis culpa consequat cillum do esse deserunt aute. Exercitation ex veniam cillum enim non aliquip cupidatat dolor consequat magna id. Adipisicing laborum laboris reprehenderit consequat duis dolore cupidatat eu. Laboris in minim amet ut dolore duis duis officia consectetur consectetur et cillum. Dolore est sit labore enim. Velit sint aliquip amet nisi non proident cillum incididunt laborum.
-            </p>
-            <p style={paragraph1}>
-                Nulla dolor reprehenderit magna qui esse duis culpa consequat cillum do esse deserunt aute. Exercitation ex veniam cillum enim non aliquip cupidatat dolor consequat magna id. Adipisicing laborum laboris reprehenderit consequat duis dolore cupidatat eu. Laboris in minim amet ut dolore duis duis officia consectetur consectetur et cillum. Dolore est sit labore enim. Velit sint aliquip amet nisi non proident cillum incididunt laborum.
-            </p>
-        </div>
+        <ScrollComponent />
     </main>
-);
+  )
+}
 
 export default scrollEffect;
